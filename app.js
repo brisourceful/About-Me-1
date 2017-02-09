f20bfad297c5f4809ba28001e19a28c0d7c77d42
Chris Closser <chrisclosser1@gmail.com>
2017-02-08 20:46:22 -0800

    added more

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
-
-var takeQuiz = confirm('Hello, would you like to take a short quiz about me?');
+// THE FIRST LINE SHOULD ALWAYS BE...'use strict'
+var takeQuiz = confirm('Hello, would you like to take a short quiz about me Chris?');
 
 if(takeQuiz) {
   console.log('Awesome! Lets get started!');
@@ -10,54 +10,64 @@ if(takeQuiz) {
   document.write('<h1>Too bad starting anyway!</h2>');
 }
 
+// first question
 var yesNo = 'Please answer with a Y or N. \n';
-
-var responseA = prompt(yesNo + 'Do I have a dog?');
+var askA = 'Do I have a dog?';
+var responseA = prompt(yesNo + askA);
 
 if(responseA.toUpperCase() === 'Y') {
   console.log('Sorry I do not have a dog.');
-  document.write('<h2>Sorry I do not have a dog.</h2>');
+  document.write(askA + '<h2>Sorry I do not have a dog.</h2>');
 } else {
-  console.log('You are correct!');
-  document.write('<h2>You are correct!</h2>');
+  console.log('You are correct! I do not have a dog.');
+  document.write(askA + '<h2>You are correct!</h2>');
 }
 
-var responseB = prompt(yesNo + 'Am I older than 40?');
+// Q #2
+var askB = 'Am I older than 40?';
+var responseB = prompt(yesNo + askB);
 
 if(responseB.toUpperCase() === 'Y') {
   console.log('Shame on you! Not quite.');
-  document.write('<h3>Shame on you! Not quite.</h3>');
+  document.write(askB + '<h3>Shame on you! Not quite.</h3>');
 } else {
   console.log('That\'s right, not there yet!');
-  document.write('<h3>That\'s right, not there yet!</h3>');
+  document.write(askB + '<h3>That\'s right, not there yet!</h3>');
 }
 
-var respnseC = prompt(yesNo + 'Do I like pizza?');
+// Q #3
+var askC = 'Do I like pizza?';
+var respnseC = prompt(yesNo + askC);
 
 if (respnseC.toUpperCase() === 'Y') {
   console.log('Hell yes I do! It does not like me however.');
-  document.write('<h4>Hell yes I do! It does not like me however.</h4>');
+  document.write(askC + '<h4>Hell yes I do! It does not like me however.</h4>');
 } else {
   console.log('Come on! Who doesn\'t like pizza?');
-  document.write('<h4>Come on! Who doesn\'t like pizza?</h4>');
+  document.write(askC + '<h4>Come on! Who doesn\'t like pizza?</h4>');
 }
 
-var responseD = prompt(yesNo + 'Do I like to gamble?');
+// Q #4
+var askD = 'Do I like to gamble?';
+var responseD = prompt(yesNo + askD);
 
 if (responseD.toUpperCase() === 'Y') {
   console.log('No I do not!');
-  document.write('<h5>No I do not!</h5>');
+  document.write(askD + '<h5>No I do not!</h5>');
 } else {
   console.log('Right it\'s bad enough all of the money that leaves my possession and I get something.');
-  document.write('<h5>Right it\'s bad enough all of the money that leaves my possession and I get something.</h5>');
+  document.write(askD + '<h5>Right it\'s bad enough all of the money that leaves my possession and I get something.</h5>');
 }
 
-var responseE = prompt(yesNo + 'Is it true that i have been to the moon?');
+// Q #5
+var askE = 'Is it true that i have been to the moon?';
+var responseE = prompt(yesNo + askE);
 
 if(responseE.toUpperCase() === 'Y') {
   console.log('I wish! 12 people have been on the moon and no one since 1972.');
-  document.write('<h6>I wish! 12 people have been on the moon and no one since 1972.</h6>');
+  document.write(askE + '<h6>I wish! 12 people have been on the moon and no one since 1972.</h6>');
 } else {
   console.log('Yup you are right only 12 people  have been on the moon and I am not one.');
-  document.write('<h6>Yup you are right only 12 people  have been on the moon and I am not one.</h6>');
+  document.write(askE + '<h6>Yup you are right only 12 people  have been on the moon and I am not one.</h6>');
 }
+// done...print the page
